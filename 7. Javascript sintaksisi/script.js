cf5ab7e225c92aa3207abe434bb4fd2153887b93
Ulugbek Samigjonov <samigjonov@gmail.com>
2025-01-19 21:54:13 +0500

    7. Javascript sintaksisi

---
+++ 7. Javascript sintaksisi/script.js
@@ -0,0 +1,9 @@
+// First variable
+let m2yVariable = 10;
+let MyVariable = 20;
+/*
+    Bu qatordagi kodlar ozgaruvchilarni
+    ekranga chiqaradi.
+*/
+console.log(m2yVariable);
+console.log(MyVariable);
