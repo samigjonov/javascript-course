6080e4be467b342e537893d50599fc606883faa5
Ulugbek Samigjonov <samigjonov@gmail.com>
2025-01-23 10:15:27 +0500

    9. Javascriptdagi malumot turlari

---
+++ 9. Javascriptdagi ma'lumot turlari/script.js
@@ -0,0 +1,16 @@
+const numberVariable = 10;        // Number
+console.log('numberVariable', numberVariable);
+console.log('typeof numberVariable', typeof numberVariable);
+const stringVariable = "salom";   // String
+console.log('stringVariable', stringVariable);
+const booleanVariable = true;      // Boolean
+console.log('booleanVariable', booleanVariable);
+console.log('typeof booleanVariable', typeof booleanVariable);
+let undefinedVariable;             // Undefined
+console.log('undefinedVariable', undefinedVariable);
+const nullVariable = null;      // Null
+console.log('nullVariable', nullVariable);
+const bigIntVariable = 123456789012345678901234567890123456789n; // BigInt
+console.log('bigIntVariable', bigIntVariable);
+const symbolVariable = Symbol("uniqueId"); // Symbol
+console.log('symbolVariable', symbolVariable);
