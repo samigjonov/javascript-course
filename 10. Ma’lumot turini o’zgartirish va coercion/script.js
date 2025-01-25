662b170f3c39819dbfd13c4416d09ad9a5c315f5
Ulugbek Samigjonov <samigjonov@gmail.com>
2025-01-25 20:16:18 +0500

    10. Malumot turini ozgartirish va coercion

---
+++ 10. Ma’lumot turini o’zgartirish va coercion/script.js
@@ -0,0 +1,48 @@
+// const age = "27";
+// const ageNumber = Number(age);
+// console.log(ageNumber);
+// const amount = "123.45";
+// console.log(Number(age));
+// console.log(parseInt(age));
+// console.log(parseFloat(age));
+
+// console.log(Number(amount));
+// console.log(parseInt(amount));
+// console.log(parseFloat(amount));
+
+// const firstname = "Ulugbek";
+// console.log(Number(firstname));
+// console.log(parseInt(firstname));
+// console.log(parseFloat(firstname));
+
+// const age = 27;
+// console.log(String(age));
+// console.log(age.toString());
+
+// console.log(Boolean(0));
+// console.log(Boolean(1));
+
+// console.log(5 + "10");
+// console.log("5" * 2);
+// console.log("5" - 2);
+
+// console.log(true + 1);
+// console.log(false + 5);
+
+// console.log(Boolean(false));      // false
+// console.log(Boolean(0));          // false
+// console.log(Boolean(-0));         // false
+// console.log(Boolean(""));         // false (empty string)
+// console.log(Boolean(null));       // false
+// console.log(Boolean(undefined));  // false
+// console.log(Boolean(NaN));        // false
+// console.log(Boolean(0n));
+
+console.log(Boolean(true));       // true
+console.log(Boolean(1));          // true
+console.log(Boolean(-1));         // true
+console.log(Boolean("hello"));    // true (non-empty string)
+console.log(Boolean(" "));        // true (string with a space)
+console.log(Boolean([]));         // true (empty array)
+console.log(Boolean({}));         // true (empty object)
+
